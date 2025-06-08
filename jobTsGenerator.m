4fb94c8a733c59a64d218b8cdc30ed00dfd3bd2d
Ifeoluwa Andrew Omole <iomole3@gmail.com>
2025-06-08 14:42:07 +0100

    receive timeseries data. setup inputs for SJN and Priority scheduling

---
+++ jobTsGenerator.m
@@ -0,0 +1,40 @@
+numJobs = 20; % increase this value for bigger sample size
+jobArray = Job.empty;
+
+% get last time step
+last = 0 + 0.5*(numJobs - 1);
+
+% job arrival times in steps of 0.5
+arrivalTime = (0:.5:last)';
+
+% create array of Jobs
+for i = 1:numJobs
+    duration = rand() * 1;
+    requiredResources = randi([1, 4]);
+    priority = randi([1, 3]);
+    jobArray(end+1) = Job(duration, requiredResources, priority, arrivalTime(i,:));
+end
+
+% convert Job array to 4xnumJobs MATLAB array
+jobMatArray = zeros(numJobs, 4);
+for i = 1:numJobs
+    jobMatArray(i, :) = [
+        jobArray(i).arrivalTime; 
+        jobArray(i).duration;
+        jobArray(i).requiredResources; 
+        jobArray(i).priority];
+end
+
+% split array into datavals and timevals
+datavals = jobMatArray(:, 2:end);
+timevals = jobMatArray(:, 1);
+
+% create time series
+jobTsFCFS = timeseries(datavals, timevals);
+jobTsSJN = timeseries(sortrows(datavals, 1), timevals);
+jobTsPriority = timeseries(sortrows(datavals, 3), timevals);
+
+% save time series
+save("jobTsFileFCFS.mat", "jobTsFCFS");
+save("jobTsFileSJN.mat", "jobTsSJN");
+save("jobTsFilePriority.mat", "jobTsPriority");
